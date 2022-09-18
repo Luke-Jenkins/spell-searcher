3e3fe700c0f30ad726683c21c19d59eeb08adbe3
Luke-Jenkins <luke.b.jenkins7@gmail.com>
2022-09-18 00:49:35 -0500

    Added option for searching by description and updated help texts.

--- cli.py
+++ cli.py
@@ -1,13 +1,16 @@
 # sets ups the CLI search tool for the user
 import click
+from pathlib import Path
+import os
+# import csv
 
 
-# class Config(object):
-#     def __init__(self):
-#         self.verbose = False
+class Config(object):
+    def __init__(self):
+        self.db_path = Path(os.getcwd() + r'/spells.csv')
 
 
-# pass_config = click.make_pass_decorator(Config, ensure=True)
+pass_config = click.make_pass_decorator(Config, ensure=True)
 
 
 # @click.group()
@@ -23,17 +26,20 @@ import click
 
 @click.command()
 @click.option(
-    '--string', default='World',
-    help='This is the thing that is greeted.')
-@click.option(
-    '--repeat', default=1,
-    help='How many times you should be greeted.')
-@click.argument('out', type=click.File('w'), default='-', required=False)
-# @pass_config
-def scry(string, repeat, out):
-    """This script greets you"""
+    '--description', default=False,
+    help='If true, searches by description.')
+# @click.option(
+#     '--repeat', default=1,
+#     help='How many times you should be greeted.')
+@click.argument('term')
+@pass_config
+def scry(Config, description, term):
+    """This script searches for a spell.\n
+    By default, spells are searched by title."""
     # if config.verbose:
     #     click.echo('We are in verbose mode.')
     # click.echo(f'Home directory is {config.home_directory}')
-    for x in range(repeat):
-        click.echo(f'Hello {string}!', file=out)
+    if description is True:
+        click.echo('Search by description.')
+    else:
+        click.echo('Search by title.')
